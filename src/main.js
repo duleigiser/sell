ac9bec1c6ba87f759c2202d7faa0716ade35e28e
dulei <564779666@qq.com>
2017-02-20 11:27:47 +0800

    引入router resource

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
+import goods from 'components/goods/goods'
+import 'common/stylus/index.styl'
+Vue.use(VueRouter)
+Vue.use(VueResource)
+let app = Vue.extend(
+  App
+)
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+let router = new VueRouter({
+  linkActiveClass: 'active'
 })
+router.map({
+  '/goods': {
+    component: goods
+  }
+})
+router.start(app, '#app')
